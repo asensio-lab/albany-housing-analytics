c6272bb19ccc2b0389eb961f4056dc52d956c8a4
olivia <oliviafiol2@gmail.com>
2019-06-27 15:42:16 -0400

    updates

--- property_scraper/scraper.py
+++ property_scraper/scraper.py
@@ -9,31 +9,38 @@ import time
 ############################################################
 #Replace with source of addresses, your api key, and the start and end index within the source of addresses
 #likely best not to retrieve more than 1000 addresses at once due to possibility of failure
-df = pd.read_csv('~/Dropbox/CDS-2019-AlbanyHub/Processed-Data/all_addresses.csv')
-header = {"apikey":"8adf54d74bfe18c9c634183af053e5b8", "accept":"application/json"}
-i_start=2001
-i_end  = i_start+1000
+df = pd.read_csv('~/Dropbox/CDS-2019-AlbanyHub/ToDatabase/addr_junct_table.csv')
+df['line1'] = df['Address']
+df['line2'] = 'Albany, GA'
+header = {"apikey":"820099b75df5e1a78a738c8a1ed7b5ef", "accept":"application/json"}
+i_start=7501
+i_end  = i_start+500
 ############################################################
 addresses = df[['line1', 'line2']]
 prop_info = []
 
 
 i=i_start
+no_data_count = 0
 while i<i_end:
     address1 = addresses.loc[i, 'line1']
     address2 = addresses.loc[i, 'line2']
     parameters = { 'address1':address1, 'address2':address2}
     response = requests.get("https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile", headers=header, params=parameters)
     #SUCCESS
+    if i % 100 == 0:
+        time.sleep(30)
     if response.status_code==200:
         i=i+1
         prop_info.append(response.json()['property'][0])
     #FAILURE
     else:
-        print("Unknown issue: Sleeping for 30s to see if error resolves on its own")
-        print("Failed at", str(i))
-        print(response.text)
-        time.sleep(30)
+        #print("Unknown issue: Sleeping for 30s to see if error resolves on its own")
+        #print("Failed at", str(i))
+        #print(response.text)
+        no_data_count = no_data_count+1
+        prop_info.append([])
+        i = i+1
     if i % 100 ==0:
         print(i, "out of",i_end)
 
