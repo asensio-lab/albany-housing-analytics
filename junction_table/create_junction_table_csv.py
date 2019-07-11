c93fbdc3f038a19693d5b7bd33f4d6c89268a1c7
mer132 <mer132@pitt.edu>
2019-07-11 13:49:14 -0400

    commit

--- junction_table/create_junction_table_csv.py
+++ junction_table/create_junction_table_csv.py
@@ -1,20 +1,23 @@
 import pandas as pd
 from fix_addresses_master import *
 #Creates the junction table based on three files from the geocoding folder
-IN_PATH = '~/Dropbox/CDS-2019-AlbanyHub/geocoding/'
-OUT_PATH = '~/Dropbox/CDS-2019-AlbanyHub/ToDatabase/'
+#The junction table consists of address, lat/lon, tract, blockgroup, and a unique ID identifier
+#Combines all addresses which have been geocoded, and replaces the files which were geocoded incorrectly with their fixed versions
+
+IN_PATH = '~/Dropbox/CDS-2019-AlbanyHub/geocoding/' #read files from
+OUT_PATH = '~/Dropbox/CDS-2019-AlbanyHub/ToDatabase/' #output junction table to 
 #read in files
-df1 = pd.read_csv(IN_PATH+"geo_joined_wtracts.csv")            #original addresses, with some errors
+df1 = pd.read_csv(IN_PATH+"geo_joined_wtracts.csv")            #addresses retrieved from utilities, with some errors
 df2 = pd.read_csv(IN_PATH+"geo_joined_wtracts_resolved.csv")   #The fixed versions of those errors
-df3 = pd.read_csv(IN_PATH+"mismatched_housing_resolved.csv")   #geocoded addresses from housing which were not in the originals
+df3 = pd.read_csv(IN_PATH+"mismatched_housing_resolved.csv")   #addresses retrieved from housing which have no data from utilities
 
 #merge columns from fixed addresses into original
 old_name_series = df2['Address'] #addresses which were geocoded incorrectly
-df_fixed = df2.drop(columns=['Address']).rename(columns={'fixed_addr':'Address'}) #addresses after fixed
+df_fixed = df2.drop(columns=['Address']).rename(columns={'fixed_addr':'Address'}) #drop the 'original address' and replace with the fixed name
 all0 = df1[~df1['Address'].isin(old_name_series)] # drop columns whose address is in old_name_series (ie, were geocoded incorrectly)
-df = pd.concat([all0, df_fixed, df3],sort=False) #ALL 3 datasets combined
+df = pd.concat([all0, df_fixed, df3],sort=False) #combine correct utilities, fixed utilities, and addresses from housing
 
-#split coordinates into x and y
+#split lat, lon coordinates into x and y
 def splitloc1(series):
     a = series.split(',')
     return a[0]
@@ -27,12 +30,13 @@ df['Ycoord'] = df['addrlocat'].apply(splitloc2)
 #specify the fields we want to keep, and their names
 junct = df[['Address', 'Xcoord', 'Ycoord', 'TRACTCE10', 'BLOCKCE10']].rename(columns={'TRACTCE10':'Tract', 'BLOCKCE10':'BlockGroup'})
 #add fields for unknown and suppressed address
+#unknown and suppressed addresses are codes for housing projects, not found is used to code addresses which were not in the junction table
 junct = junct.append({'Address':'UNKNOWN ADDRESS'}, ignore_index=True)
 junct = junct.append({'Address':'SUPPRESSED ADDRESS'}, ignore_index=True)
 junct = junct.append({'Address':'NOT FOUND'}, ignore_index=True)
-#modify the addresses
+#modify the addresses using the master typo fixer dictionary
 junct['Address'] = fix_series(junct['Address'])
 junct = junct.drop_duplicates(subset ="Address", 
-                     keep = 'last')
-junct.index= range(len(junct))
-junct.to_csv(OUT_PATH+"addr_junct_table.csv")
+                     keep = 'last')# drop duplicate addresses
+junct.index= range(len(junct)) #index is the ID - one value for each row
+junct.to_csv(OUT_PATH+"addr_junct_table.csv") #output to csv
--- junction_table/map_addr_key_housing.py
+++ junction_table/map_addr_key_housing.py
@@ -2,7 +2,7 @@ import pandas as pd
 from fix_addresses_master import *
 #This file assigns the address key based on the junction table to each entry in the housing projects data set
 #outputs:   TotalHouse.csv - same file with junction values added
-#           house_notfound.csv = the addresses which were not found in the junction table 
+#           house_notfound.csv = the addresses which were not found in the junction table (coded as NOT FOUND)
 #########################################
 #Specify paths below this line
 #########################################
@@ -14,27 +14,27 @@ df_junction_table = pd.read_csv('~/Dropbox/CDS-2019-AlbanyHub/ToDatabase/addr_ju
 output_path = "~/Dropbox/CDS-2019-AlbanyHub/ToDatabase/"
 #########################################
 df_junction_table.columns = ['PrimaryID', 'Address', 'Xcoord', 'Ycoord', 'Tract', 'BlockGroup', 'Block']
-my_dict = df_junction_table.set_index('Address').to_dict()['PrimaryID']#maps address to key
-s_housing_projects = df_housing_projects['Address']#list of addresses per charge
+my_dict = df_junction_table.set_index('Address').to_dict()['PrimaryID']#maps each address to its id
+s_housing_projects = df_housing_projects['Address']#lists all addresses in housing projects
 
 #Drop unneccesary columns
 df_housing_projects.drop(['City', 'State', 'XY Coordinates'], inplace = True, axis = 1)
 
-
-primaryid_list1 = [None] * len(s_housing_projects)
+#For each address in housing projects, pull address id from dictionary
+primaryid_list1 = [None] * len(s_housing_projects) #the id for each address
 notfound_dict = {}
 counter = 0
 s_housing_projects = fix_series(s_housing_projects)   #Fix typos using fix_addresses_master.py
-#For each address in utilities, pull address id from dictionary
+#loop over each address in the dataset and retrieve its id
 for i in range(0, len(s_housing_projects)):
-    try: #get index of where addresses match
-        primaryid_list1[i] = my_dict[s_housing_projects[i]]
+    try: 
+        primaryid_list1[i] = my_dict[s_housing_projects[i]] #the address has a corresponding id
     except KeyError:
-        primaryid_list1[i] = my_dict['NOT FOUND']
+        primaryid_list1[i] = my_dict['NOT FOUND'] #the address does not have a corresponding id: code as not found and keep track of the address
         notfound_dict[i] = s_housing_projects[i]
     counter += 1
 #output to file
-df_housing_projects['PrimaryID'] = pd.Series(primaryid_list1)
+df_housing_projects['PrimaryID'] = pd.Series(primaryid_list1) #set the primary id field
 df_housing_projects.to_csv(output_path+"TotalHouse_v03.csv")
-df2 = pd.DataFrame(data={'addr':list(notfound_dict.values()), 'loc':list(notfound_dict.keys())})
+df2 = pd.DataFrame(data={'addr':list(notfound_dict.values()), 'loc':list(notfound_dict.keys())})#format the list of not found addresses as a pandas dataframe
 df2.to_csv(output_path+"house_notfound.csv")
